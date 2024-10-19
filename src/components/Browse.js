09f6c7c8c62dedbb7e8070d140d3c512c4e6eca2
navaloli007 <navaloli007@gmail.com>
2024-10-19 14:20:28 +0530

    creating the maincontainer with video and description.

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,25 +1,23 @@
-import React, { useEffect } from 'react'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch } from 'react-redux'
-import { addNowPlayingMovies } from '../utils/moviesSlice'
+import MainContainer from './MainContainer';
+import SecondaryContainer from './SecondaryContainer';
 
 const Browse = () => {
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async () => {
-    // const data = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', API_OPTIONS);
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-    const json = await data.json();
-    console.log(json);
-    dispatch(addNowPlayingMovies(json.results));
-  };
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {/* 
+        MainCOntainer
+          - VideoBAckground
+          - VideoTitle
+        SecondryContainer
+          - MoviesList * n
+          - cards * n
+      */}
     </div>
   )
 }
