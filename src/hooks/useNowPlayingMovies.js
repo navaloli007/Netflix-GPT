b32887e6cc51b119199b7c9f5192e57beb361782
navaloli007 <navaloli007@gmail.com>
2024-10-24 22:13:20 +0530

    fetch popular movies and create hook for the same.

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -6,7 +6,6 @@ import { addNowPlayingMovies } from '../utils/moviesSlice'
 const useNowPlayingMovies = () => {
     const dispatch = useDispatch();
     const getNowPlayingMovies = async () => {
-        // const data = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', API_OPTIONS);
         const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
         const json = await data.json();
         dispatch(addNowPlayingMovies(json.results));
