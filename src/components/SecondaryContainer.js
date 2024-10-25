a910f45d69afbe36c110e09e5570f1bad6e0dc71
navaloli007 <navaloli007@gmail.com>
2024-10-25 21:58:05 +0530

    display the popular movies.

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -8,7 +8,7 @@ const SecondaryContainer = () => {
         <div className='bg-black'>
             <div className='-mt-72 pl-12 relative z-20'>
                 <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-                <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+                <MovieList title={"Popular"} movies={movies.popularMovies} />
                 <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
                 <MovieList title={"Upcoming"} movies={movies.nowPlayingMovies} />
                 <MovieList title={"Action"} movies={movies.nowPlayingMovies} />
