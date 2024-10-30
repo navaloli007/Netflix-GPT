73db07775a4f090108579e2c2f4216e079fb549d
navaloli007 <navaloli007@gmail.com>
2024-10-30 21:38:57 +0530

    create search input and button.

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,7 +1,12 @@
 import React from 'react'
 const GptSearchBar = () => {
   return (
-    <div>GptSearchBar</div>
+    <div className='pt-[10%] flex justify-center'>
+      <form className='w-1/2 bg-black grid grid-cols-12'>
+        <input type='text' className='p-4 m-4 col-span-9' placeholder='WHat would you like to watch today ?' />
+        <button className='col-span-3 m-4 py-2 px-2 bg-red-700 text-white rounded-lg'>Search</button>
+      </form>
+    </div>
   )
 }
 
