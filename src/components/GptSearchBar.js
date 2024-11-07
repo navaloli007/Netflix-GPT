613d6686f349ba7f7c1a4e016f5287377bc4cedc
navaloli007 <navaloli007@gmail.com>
2024-11-07 17:51:31 +0530

    show the searched movie list using api and make the slices of it.

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,19 +1,35 @@
 import React, { useRef } from 'react'
 import lang from '../utils/languageConstants'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import openai from '../utils/openAi';
+import { API_OPTIONS } from '../utils/constants';
+import { addGptMovieResult } from '../utils/gptSlice';
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef();
+  const dispatch = useDispatch();
+
+  // Serach movie in TMDB;
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`, API_OPTIONS);
+    const json = await data.json();
+    return json.results;
+  };
   const handleGptSearchClick = async () => {
     let gptQuery = "Act as a Movie Recommendation system and suggest some movie for the query : " + searchText.current.value + ". only give me the name of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmal, Koi Mil Gaya";
     const chatCompletion = await openai.chat.completions.create({
       messages: [{ role: 'user', content: gptQuery }],
       model: 'gpt-3.5-turbo',
     });
-    console.log(chatCompletion.choices);
-  }
+    if (!chatCompletion.choices) {
+      // ToDO: Write Error Handling
+    }
+    const gptMovies = chatCompletion.choices?.[0]?.message?.content.split(",");
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+    const tmdbResults = await Promise.all(promiseArray);
+    dispatch(addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults }));
+  };
   return (
     <div className='pt-[10%] flex justify-center'>
       <form className='w-1/2 bg-black grid grid-cols-12' onSubmit={(e) => e.preventDefault()}>
--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -1,16 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 const gptSlice = createSlice({
     name: "gpt",
     initialState: {
-        showGptSearch: false
+        showGptSearch: false,
+        movieNames: null,
+        movieResults: null
     },
     reducers: {
         toogleGptSearchView: (state, action) => {
             state.showGptSearch = !state.showGptSearch;
         },
+        addGptMovieResult: (state, action) => {
+            const { movieNames, movieResults } = action.payload;
+            state.movieNames = movieNames;
+            state.movieResults = movieResults;
+        }
     },
 });
 
-export const { toogleGptSearchView } = gptSlice.actions;
+export const { toogleGptSearchView, addGptMovieResult } = gptSlice.actions;
 export default gptSlice.reducer;
