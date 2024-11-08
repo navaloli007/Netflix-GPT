66b57237d89af21b20f27ab06e34f418cde29560
navaloli007 <navaloli007@gmail.com>
2024-11-08 21:47:10 +0530

    some background adjustment for movie suggestions.

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -6,7 +6,7 @@ const GptMovieSuggestions = () => {
     const { movieResults, movieNames } = useSelector((store) => store.gpt);
     if (!movieNames) return null;
     return (
-        <div className='p-4 m-4 bg-black text-white'>
+        <div className='p-4 m-4 bg-black text-white bg-opacity-80'>
             <div>
                 {
                     movieNames.map((movieName, index) => (
